b4deed13aa57c51cbfd92a2c1da7ff24bebe3863
Daniel <daniele.sanchez.dev@gmail.com>
2023-11-15 18:10:56 -0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,65 @@
-import './App.css'
-import { Spotify } from 'react-spotify-embed'
-
+import "./App.css";
+import { Spotify } from "react-spotify-embed";
+import barb from "./assets/barb.gif";
+import { useRef } from "react";
 
 function App() {
+  const topRef = useRef(null);
+  const splitTopRef = useRef(null);
+  const splitBottomRef = useRef(null);
+  const bottomRef = useRef(null);
+
+  const handleClick = (section) => {
+    section.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
-      <div className='main-container'>
-        <h1>Hey there, Nata</h1>
-        <p>Here's a rose</p>
-        <span className='rose'>&#127801;</span>
-        <p>And a song for you...</p>
-        <div>
-          <Spotify link="https://open.spotify.com/track/5T4ngPEPvNmEFkSDa7cxVw?si=fdd6599e75a2454c&nd=1" />
-
+      <div className="main-container">
+        <div id="top" ref={topRef}>
+          <p>Hey, what's your name?</p>
+          <button
+            className="arrow-button"
+            onClick={() => {
+              handleClick(splitTopRef);
+            }}
+          >
+            Continue<span className="arrow"></span>
+          </button>
+        </div>
+        <div id="split-top" ref={splitTopRef}>
+          <p>Just kidding, you're Nata, beautiful!</p>
+          <button
+            className="arrow-button"
+            onClick={() => {
+              handleClick(splitBottomRef);
+            }}
+          >
+            Continue<span className="arrow"></span>
+          </button>
+        </div>
+        <div id="split-bottom" ref={splitBottomRef}>
+          <p>Here's a dancing Barbie</p>
+          <img className="split-img" src={barb} alt="" />
+          <button
+            className="arrow-button"
+            onClick={() => {
+              handleClick(bottomRef);
+            }}
+          >
+            Continue<span className="arrow"></span>
+          </button>
+        </div>
+        <div id="bottom" ref={bottomRef}>
+          <p>And a new song for you...</p>
+          <div>
+            <Spotify link="https://open.spotify.com/track/4FU7pMmM75ihU8SRZDZKXP?si=57be2ca106294663" />
+          </div>
+          <p>Can't wait to be near you!</p>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
