34b87e120a2ec36263522c06353f08f0889df5b6
Daniel Sánchez <daniele.sanchez.dev@gmail.com>
2023-11-17 12:12:01 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { Spotify } from "react-spotify-embed";
-import barb from "./assets/barb.gif";
+import n from "./assets/n.png";
 import { useRef } from "react";
 
 function App() {
   const topRef = useRef(null);
-  const splitTopRef = useRef(null);
-  const splitBottomRef = useRef(null);
   const bottomRef = useRef(null);
 
   const handleClick = (section) => {
@@ -17,30 +15,7 @@ function App() {
     <>
       <div className="main-container">
         <div id="top" ref={topRef}>
-          <p>Hey, what's your name?</p>
-          <button
-            className="arrow-button"
-            onClick={() => {
-              handleClick(splitTopRef);
-            }}
-          >
-            Continue<span className="arrow"></span>
-          </button>
-        </div>
-        <div id="split-top" ref={splitTopRef}>
-          <p>Just kidding, you're Nata, beautiful!</p>
-          <button
-            className="arrow-button"
-            onClick={() => {
-              handleClick(splitBottomRef);
-            }}
-          >
-            Continue<span className="arrow"></span>
-          </button>
-        </div>
-        <div id="split-bottom" ref={splitBottomRef}>
-          <p>Here's a dancing Barbie</p>
-          <img className="split-img" src={barb} alt="" />
+          <img className="split-img" src={n} alt="" />
           <button
             className="arrow-button"
             onClick={() => {
@@ -51,11 +26,10 @@ function App() {
           </button>
         </div>
         <div id="bottom" ref={bottomRef}>
-          <p>And a new song for you...</p>
+          <p>I'm here for and with you...</p>
           <div>
-            <Spotify link="https://open.spotify.com/track/4FU7pMmM75ihU8SRZDZKXP?si=57be2ca106294663" />
+            <Spotify link="https://open.spotify.com/track/78DwRIo6Vj6wqteyerGetr?si=e76d8afde62e4c7f" />
           </div>
-          <p>Can't wait to be near you!</p>
         </div>
       </div>
     </>
